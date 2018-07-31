45d73cff3c58c4006b4acec5e6b1659fb629dd7a
LaurenWorkosky <19lworkosky@student.dasd.org>
2018-07-31 14:32:07 +0000

    Done.

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,17 @@
 require 'bundler'
+require './models/model'
 Bundler.require
 
 class MyApp < Sinatra::Base
-
   get '/' do
-    erb :index
+    "This is your home page"
   end
-
+  
+  get '/hello' do
+    "hello everyone, I hope you are having a great day. I hope you drink lots of water and sleep well tonight. Ok bye!"
+  end
+  
+  get '/itslit' do
+    erb:index
+  end
 end
