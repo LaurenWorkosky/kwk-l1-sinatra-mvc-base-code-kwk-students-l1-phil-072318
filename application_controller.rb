ca9f47800834e42b6abda3d2253d2689c4203ebd
LaurenWorkosky <19lworkosky@student.dasd.org>
2018-08-02 13:06:52 +0000

    automatically backed up by learn

--- application_controller.rb
+++ application_controller.rb
@@ -7,17 +7,7 @@ class MyApp < Sinatra::Base
     "Welcome to Lauren's website. /pictures for pictures. /lauren for fun facts. /hello/:name for a fun surprise and /survey for a food survey. /"
   end
   
-  get '/' do
-    erb:index
-  end
   
-  get '/food' do
-    erb:food
-  end
-  
-  get '/fitness' do
-    erb:fitness
-  end
   
 
 
